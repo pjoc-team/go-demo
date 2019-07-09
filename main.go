b81d22cc27e2fa420ec3eb82d9e1fac23d1d23d8
blademainer <blademainer@gmail.com>
2019-07-09 21:56:26 +0800

    fixes #3
    update organization

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+" by blademainer@pjoc \n")
+	fmt.Fprintf(w, "Hello from:  "+title+" by blademainer@pjoc-team \n")
 }
 
 func main() {
